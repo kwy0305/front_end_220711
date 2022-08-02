ecea912b14975596d416bc71f8306c08927e414e
kwy0305 <dnjsdudjj@naver.com>
2022-08-02 18:23:29 +0900

    fldorxm
    .JSX 기본문법
    .react 컴포넌트
    .props
    .객체 비구조화 할당

---
+++ learn-react/src/components/Counter.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Counter() {
+  // useState(초기값) : [초기값, set함수]를 반환한다.
+  //   => set함수를 통해 새로운 값을 업데이트하면 렌더링이 다시 일어난다.
+  const [count, setCount] = useState(0);
+  const [increaseNum, setIncreaseNum] = useState(0);
+
+  console.log(count);
+  function add() {
+    setCount(count + increaseNum);
+  }
+
+  function sub() {
+    // 최적화할 때 사용
+    // 함수형 업데이트 : set함수의 인자로 콜백함수를 전달하면 콜백함수의 첫번째 인자로 최신 상태값을 전달
+    setCount((num) => num - increaseNum);
+  }
+
+  // 이벤트 객체를 받아서 증가량을 업데이트
+  function onchangeIncrease(e) {
+    // input 태그의 값이 문자열로 전달되기 때문에 숫자로 변환
+    let increaseNum = parseInt(e.target.value);
+    setIncreaseNum(increaseNum);
+  }
+
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={add}>증가</button>
+      <button onClick={sub}>감소</button>
+      증가량 : <input type="number" onChange={onchangeIncrease} />
+    </>
+  );
+}
+
+export default Counter;
---
+++ learn-react/src/components/MovieList.js
@@ -0,0 +1,50 @@
+let movieList = [
+  {
+    id: 1,
+    title: "범죄도시2",
+    actors: "마동석, 손석구",
+    star: 4.5,
+  },
+  {
+    id: 2,
+    title: "외계+인",
+    actors: "류준열, 김우빈",
+    star: 4,
+  },
+  {
+    id: 3,
+    title: "마녀 2",
+    actors: "신시아, 김다미",
+    star: 5,
+  },
+];
+
+// 전달받은 props를 매개변수에서 비구조화 할당
+function Movie({ item }) {
+  //props : { item : {title : "...", actors : "...", start : "..."} }
+  const { title, actors, star } = item;
+  return (
+    <li>
+      <h2>{title}</h2>
+      <div>{actors}</div>
+      <div>{star}</div>
+    </li>
+  );
+}
+
+function MovieList() {
+  return (
+    <ul>
+      {/* 
+          jsx 안에 배열값을 넣으면 순서대로 출력된다.  
+          Array.prototype.map으로 데이터를 jsx 형식으로 만들어 새로운 배열을 반환한다.
+        */}
+      {movieList.map((movie) => {
+        // movieList의 각 요소를 movie 프로퍼티로 전달
+        return <Movie item={movie} />;
+      })}
+    </ul>
+  );
+}
+
+export default MovieList;
