a4bb34e7ac7456ec8b77f9a677300944712caf4a
kwy0305 <dnjsdudjj@naver.com>
2022-08-05 16:00:32 +0900

    투두리스트 완성 (리액트)

---
+++ todo-list/src/color.js
@@ -0,0 +1,7 @@
+import { lighten, darken } from "polished";
+
+export const main_color = "#07528b";
+
+export const hover_color = lighten(0.05, main_color);
+
+export const active_color = darken(0.05, main_color);
